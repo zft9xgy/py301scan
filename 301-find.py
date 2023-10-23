133c15ddbb856f3e7b50c132a113c7adeefc04fa
zft9xgy <zft9xgy@icloud.com>
2023-10-23 16:15:29 +0200

    Optimice get-urls-from-sitemap and finishing brand 301find

--- 301-find.py
+++ 301-find.py
@@ -1,9 +1,1 @@
-import requests
-from bs4 import BeautifulSoup
-from tqdm import tqdm
-import csv
-import os
-import concurrent.futures
-from configparser import ConfigParser
-
-# Script que buscara urls con codigos de error 301 en todas las urls proporcionadas en la lista urls.txt.
+# still in progress
--- get-urls-from-sitemap.py
+++ get-urls-from-sitemap.py
@@ -7,64 +7,63 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-# URL del sitemap desde el archivo config
-SITEMAP_URL = config['DEFAULT']['SITEMAP_URL']
-URLS_FILE = config['DEFAULT']['URLS_FILE']
+# Default Execution options
+sitemap_url = config['DEFAULT']['SITEMAP_URL']
+url_list = config['DEFAULT']['URLS_FILE']
+exclude_wpcontent = config['GETFROMSITEMAP']['EXCLUDE_WPCONTENT']
+mode = config['GETFROMSITEMAP']['MODE']
 
 
-def get_total_urls_in_sitemap(sitemap_url):
-    response = requests.get(sitemap_url)
-    soup = BeautifulSoup(response.text, "xml")
-    return len(soup.find_all("loc"))
+def set_options():
+    global mode, sitemap_url, mode
 
-
-def main():
-    # Informar al usuario sobre la URL del sitemap que se utilizará.
-    print(f"Current sitemap URL: {SITEMAP_URL}")
-
-    # Pedir confirmación para usar la URL o introducir una nueva.
+    # show current sitemap and ask for change.
+    print(f"Current sitemap URL: {sitemap_url}")
     decision = input(
         "To use another sitemap URL, paste it here or press ENTER to continue with the current one: ")
 
     if decision:
-        sitemap_url = decision.strip()  # limpiar espacios en blanco
+        sitemap_url = decision.strip()
     else:
-        sitemap_url = SITEMAP_URL
+        sitemap_url = sitemap_url
 
     # Preguntar al usuario sobre excluir URLs de imágenes (por defecto sí)
-    exclude_images = input(
-        "By default, Yoast SEO generated sitemaps contain many images. Do you want to exclude URLs containing '/wp-content/'? (yes/no [default: yes]): ")
-    if exclude_images.lower() not in ['no', 'n']:
-        exclude_images = True
+    exclude_wpcontent = input(
+        "By default, Yoast SEO generated sitemaps contain many images. Do you want to exclude URLs containing '/wp-content/'? (yes/no) [press enter to default: yes]: ")
+    if exclude_wpcontent.lower() not in ['no', 'n']:
+        exclude_wpcontent = True
     else:
-        exclude_images = False
+        exclude_wpcontent = False
 
     # Preguntar al usuario si quiere agregar o regenerar la lista
     append_to_list = input(
-        "Do you want to append the extracted URLs to the end of the list? (yes/no [default: yes][no:regenerate list]): ")
+        "Do you want to append the extracted URLs to the end of the list? (yes/no) [press enter to default: yes][no:regenerate list]): ")
     if append_to_list.lower() in ['no', 'n']:
         mode = 'w'
     else:
         mode = 'a'
 
-    # Obtener el total de URLs en el sitemap
-    total_urls = get_total_urls_in_sitemap(sitemap_url)
-    print(f"Fetching {total_urls} URLs from the sitemap...")
+
+def main():
+
+    set_options()
 
     response = requests.get(sitemap_url)
     soup = BeautifulSoup(response.text, "xml")
-
     loc_tags = soup.find_all("loc")
+    total_urls = len(loc_tags)
+
+    print(f"Fetching {total_urls} URLs from the sitemap...")
 
     # Extraer las URLs con barra de progreso y escribir en el archivo
-    with open(URLS_FILE, mode) as file:
+    with open(url_list, mode) as file:
         for loc in tqdm(loc_tags, total=total_urls, desc="Extracting URLs"):
             url = loc.text
-            if exclude_images and '/wp-content/' in url:
+            if exclude_wpcontent and '/wp-content/' in url:
                 continue
             file.write(url + "\n")
 
-    print(f"URLs have been saved to {URLS_FILE}.")
+    print(f"URLs have been saved to {url_list}.")
 
 
 if __name__ == "__main__":
